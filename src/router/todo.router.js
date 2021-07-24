1fe20c8e6a1d8a2aaf1271c63018901fec5d3d46
sleman safiah <slemansafiah43@gmail.com>
2021-07-24 20:58:06 +0300

    add get all todo function

--- src/router/todo.router.js
+++ src/router/todo.router.js
@@ -6,5 +6,6 @@ router.post("/", auth, todoController.addTodo);
 router.post("/:id", auth, todoController.updateTodo);
 router.delete("/:id", auth, todoController.deleteTodo);
 router.get("/:id", auth, todoController.getTodo);
+router.get("/", auth, todoController.getAllTodos);
 
 module.exports = router;
